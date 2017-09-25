3d363527703248f14324bc52998b1b6c83cd349b
shawnhuggins <hugginsjrshawn@gmail.com>
2017-09-24 20:06:55 -0500

    Updated file names for readability.

--- corn_calc.py
+++ corn_calc.py
@@ -3,10 +3,11 @@ import sys
 __version__ = '0.1.0'
 
 def main():
-    """
-    Entry function of the application
-    Currently only displays the version number if the -v flag is used as
-    a command line argument to show proof of concept
+    """The entry function of the application.
+
+    Current functionality is a place holder.
+
+    Example: $python3 corn_calc.py -v.
     """
     if len(sys.argv) > 1 and sys.argv[1] == '-v':
             print(f'Current version is {__version__}')
