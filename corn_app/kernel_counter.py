e7e9dbad434adfdb8888eae486c21687c9ee30c3
shawnhuggins <hugginsjrshawn@gmail.com>
2017-09-25 17:50:30 -0500

    Added test case for convert_to_black_white

--- corn_app/kernel_counter.py
+++ corn_app/kernel_counter.py
@@ -14,65 +14,86 @@ LUMA_BLUE  = 0.0722
 # The threshold determing if a pixel should be black or white based from the lumanince
 LUMA_THRESHOLD = 150
 
-def convertToBlackWhite(image, pixelMap):
-"""Converts an image into black and white based from its lumanince.
+def convert_to_black_white(image):
+    """Converts an image into black and white based from its lumanince.
 
-This function was ported and improved from the 2016 Corn Yield Senior design project.
+    Currently only supports RGB image modes.
+    
+    Args:
+        image (Image): An open Image instance.
 
-Args:
-    image (Image): An open Image instance.
-    pixelMap (PixelAccess): A buffer containing the bitmap data.
-
-Returns:
-    bool: True if the image is converted, false otherwise.
-"""
-    # RGB is currently only supported.
-    if image.mode != 'RGB':
+    Returns:
+        bool: True if the image is converted, false otherwise.
+    """
+    if not image or image.mode != 'RGB':
         return False
 
-    imageWidth, imageHeight = image.size
+    pixel_map = image.load()
 
-    for i in range(imageWidth):
-        for j in range(imageHeight):
-            newPixelValue = (0, 0, 0)
-            pixel = pixelMap[i, j]
+    image_width, image_height = image.size
+
+    for i in range(image_width):
+        for j in range(image_height):
+            new_pixel_value = (0, 0, 0)
+            pixel = pixel_map[i, j]
 
             # Calculate the total luminance of a pixel.
             luma = pixel[0] * LUMA_RED + pixel[1] * LUMA_GREEN + pixel[2] * LUMA_BLUE
- 
-            if luma > LUMA_THRESHOLD: newPixelValue = (255, 255, 255)
+            
+            # Set pixel to white if luma is larger than LUMA_THRESHOLD
+            if luma > LUMA_THRESHOLD: new_pixel_value = (255, 255, 255)
 
-            pixelMap[i, j] = newPixelValue
+            pixel_map[i, j] = new_pixel_value
+
+    if __debug__ : image.save('../debug/black_white_test.jpg')
 
     return True
-    
-if __name__ == "__main__":
-    # Execute only if the file is ran as a script.
-    # Example: python3 ip.py file/path/image file/path/output-image.
+
+def main(argv):
+    """The function called when kernel_counter.py is ran
+
+    Converts an image into a black and white format and saves the file.
+    Example: python3 kernel_counter.py file/path/image file/path/output-image.
+    Will likely be removed when testing framework is added
+
+    Args:
+        argv (list): The sys.argv list
+
+    Returns:
+        None    
+    """
     if len(sys.argv) == 3:
-        filePath     = sys.argv[1]
-        outPutPath   = sys.argv[2]
+        file_path    = sys.argv[1]
+        output_path  = sys.argv[2]
         image        = None
-        pixelMap     = None
+        pixel_map    = None
 
         try:
-            image = Image.open(filePath)
-            pixelMap = image.load()
+            image = Image.open(file_path)
+            pixel_map = image.load()
         except:
             print('Image could not be opened')
             sys.exit()
 
         print('Converting image to black and white')
-        isSuccess = convertToBlackWhite(image, pixelMap)
+        is_success = convert_to_black_white(image, pixel_map)
 
-        if isSuccess:
+        if is_success:
             print('Image successfully converted')
-            image.save(outPutPath)
+            try:
+                image.save(output_path)
+            except:
+                print('Image could not be saved. Check the output path file extension')
+
         else:
             print('Image could not be converted')
 
     else:
         print("An image path was not provided")
+    
+if __name__ == "__main__":
+    # Executes if the file is ran as a script
+    main(sys.argv)
 
 
 
