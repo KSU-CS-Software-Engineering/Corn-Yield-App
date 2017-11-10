faa5a2bf64a85bd8dd5fd35979cce82628e0e051
Shawn Huggins <hugginsjrshawn@gmail.com>
2017-11-10 13:53:57 -0600

    Add find_contours function.

--- corn_app/contour.py
+++ corn_app/contour.py
@@ -1,29 +1,39 @@
-"""Finds the contours of an image
-
-The COUNTOUR_THRESHOLD will be found from an image's
-luminosity, allowing this module to work for varyiny
-lighting conditions in the future.
-
-Module is currenlty in the experimental phase
+"""Finds the contours of kernels on the ears of corn
 
 Attributes:
     CONTOUR_COLOR (tuple): RGB color value used to draw the contours
-    COUTOUR_THRESHOLD (int): The boundry between turning a pixel white or black
+    BLOCK_SIZE (int): The pixel size of the square to find a threshold for
+    LINE_WIDTH (int): The width of contour lines
 """
-
 import cv2
 import numpy as np
 
-# Green RGB  value
-CONTOUR_COLOR = (0, 255, 0) 
-COUTOUR_THRESHOLD = 40
+# RGB color values
+GREY_SCALE_WHITE = 255
+BLACK = (0,0,0)
+GREEN = (0,255,0)
 
-#image name is currently hard coded in
-filename = 'test4.JPG'
-imL = cv2.imread(filename)
-im = imL
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,COUTOUR_THRESHOLD,255,0)
-im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(im, contours, -1, CONTOUR_COLOR, 3)
-cv2.imwrite('x' + filename, im)
+CONTOUR_COLOR = GREEN
+BLOCK_SIZE = 71
+LINE_WIDTH = 5
+
+def find_contours(image):
+    """Finds the contours of kernels on the ears of corn
+
+    Args:
+        image (openCV Image): An open Image object.
+
+    Returns:
+        Image -- An image with the contours drawn in
+    """
+    if image is None:
+        return None
+
+    imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    thres = cv2.adaptiveThreshold(imgray, GREY_SCALE_WHITE, cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,BLOCK_SIZE,0)
+    im2, contours, hierarchy = cv2.findContours(thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, contours, -1, CONTOUR_COLOR, LINE_WIDTH)
+
+    return image
+
