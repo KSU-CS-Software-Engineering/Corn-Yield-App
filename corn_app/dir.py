13ad55c986afb232d49910ffd750d6f19129e54a
Tyler Nielson <tyler.snielson@gmail.com>
2017-11-13 12:11:40 -0600

    added outputting of image and creation of OpenCV image object.

--- corn_app/dir.py
+++ corn_app/dir.py
@@ -1,6 +1,7 @@
 import json
 import os
 import contour
+import cv2
 
 json_data = json.load(open('../config.json'))
 
@@ -9,4 +10,7 @@ output_dir = json_data['contourPhotoDir']
 
 for file in os.listdir(photo_dir):
     if file.endswith('.JPG'):
-        contour.find_contours(file)
+        image = cv2.imread(os.path.join(photo_dir, file))
+        contoured_image = contour.find_contours(image)
+        os.chdir(output_dir)
+        cv2.imwrite('contours_' + file, contoured_image)
