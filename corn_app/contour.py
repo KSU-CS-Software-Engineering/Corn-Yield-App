6d8409473c19a4febd35bc7e31ae4e2fd274f55f
Ikhide Eboreime <mindofamiddlechild@gmail.com>
2017-11-15 18:04:35 -0600

    extracted yellow pixels

--- corn_app/contour.py
+++ corn_app/contour.py
@@ -17,6 +17,10 @@ CONTOUR_COLOR = GREEN
 BLOCK_SIZE = 71
 LINE_WIDTH = 5
 
+#HSV yellow color boundaries
+LOWER = [20,70,70]
+UPPER = [40,255,255]
+
 def find_contours(image):
     """Finds the contours of kernels on the ears of corn
 
@@ -24,16 +28,26 @@ def find_contours(image):
         image (openCV Image): An open Image object.
 
     Returns:
-        Image -- An image with the contours drawn in
+        yellow_hsv(Image) -- An image with the contours drawn in
+        contours(list): List of contours drawn
     """
     if image is None:
         return None
 
-    imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower_yellow = np.array(LOWER)
+    upper_yellow = np.array(UPPER)
+
+    yellow_mask = cv2.inRange(image_hsv, lower_yellow, upper_yellow)
+    yellow_hsv = cv2.bitwise_and(image, image, mask = yellow_mask)
+    yellow_bgr = cv2.cvtColor(yellow_hsv, cv2.COLOR_HSV2BGR)
+
+    imgray = cv2.cvtColor(yellow_bgr,cv2.COLOR_BGR2GRAY)
     thres = cv2.adaptiveThreshold(imgray, GREY_SCALE_WHITE, cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,BLOCK_SIZE,0)
     im2, contours, hierarchy = cv2.findContours(thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, -1, CONTOUR_COLOR, LINE_WIDTH)
+    cv2.drawContours(yellow_hsv, contours, -1, CONTOUR_COLOR, LINE_WIDTH)
 
-    return image
+    return yellow_hsv, contours
 
