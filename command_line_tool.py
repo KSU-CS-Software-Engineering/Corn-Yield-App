dc3617bc3b7d4e45442c6f0ae83ffc1a00f4cce9
Shawn Huggins <hugginsjrshawn@gmail.com>
2017-12-08 10:33:24 -0600

    Convert tabs to spaces.

--- command_line_tool.py
+++ command_line_tool.py
@@ -11,44 +11,44 @@ photo_dir = ""
 output_dir = ""
 
 def apply_mask():
-	for file in os.listdir(photo_dir):
-		if file.endswith('.JPG'):
-			image = cv2.imread(os.path.join(photo_dir, file))
-			masked_image = corn_app.mask.mask_yellow(image)
-			os.chdir(output_dir)
-			cv2.imwrite('mask_' + file, masked_image)
+    for file in os.listdir(photo_dir):
+        if file.endswith('.JPG'):
+            image = cv2.imread(os.path.join(photo_dir, file))
+            masked_image = corn_app.mask.mask_yellow(image)
+            os.chdir(output_dir)
+            cv2.imwrite('mask_' + file, masked_image)
 
 
 def apply_contours():
-	for file in os.listdir(photo_dir):
-		if file.endswith('.JPG'):
-			image = cv2.imread(os.path.join(photo_dir, file))
-			contoured_image = corn_app.contour.find_contours(image)
-			os.chdir(output_dir)
-			cv2.imwrite('contours_' + file, contoured_image)
+    for file in os.listdir(photo_dir):
+        if file.endswith('.JPG'):
+            image = cv2.imread(os.path.join(photo_dir, file))
+            contoured_image = corn_app.contour.find_contours(image)
+            os.chdir(output_dir)
+            cv2.imwrite('contours_' + file, contoured_image)
 
 
 def process():
-	for file in os.listdir(photo_dir):
-		if file.endswith('.JPG'):
-			image = cv2.imread(os.path.join(photo_dir, file))
-			contoured_image = corn_app.contour.find_contours(corn_app.mask.mask_yellow(image))
-			os.chdir(output_dir)
-			cv2.imwrite('processed_' + file, contoured_image)
+    for file in os.listdir(photo_dir):
+        if file.endswith('.JPG'):
+            image = cv2.imread(os.path.join(photo_dir, file))
+            contoured_image = corn_app.contour.find_contours(corn_app.mask.mask_yellow(image))
+            os.chdir(output_dir)
+            cv2.imwrite('processed_' + file, contoured_image)
 
 
 def main(args):
-	if args.mask is True:
-		apply_mask()
-		exit(0)
+    if args.mask is True:
+        apply_mask()
+        exit(0)
 
-	if args.contour is True:
-		apply_contours()
-		exit(0)
+    if args.contour is True:
+        apply_contours()
+        exit(0)
 
-	if args.process is True:
-		process()
-		exit(0)
+    if args.process is True:
+        process()
+        exit(0)
 
 
 parser = argparse.ArgumentParser(description='Applies a mask and contours to pictures of corn.', prog='Corn Kernel Counter Prep Application')
@@ -59,13 +59,13 @@ parser.add_argument('-p', '--process', action='store_true', default=False, help=
 args = parser.parse_args()
 
 try:
-	json_data = json.load(open('config.json'))
+    json_data = json.load(open('config.json'))
 except IOError:
-	print('There was an error opening the \'config.json\' file, or it does not exist. Please create one in a similar structure to \'sample_config.json\'')
-	sys.exit(1)
+    print('There was an error opening the \'config.json\' file, or it does not exist. Please create one in a similar structure to \'sample_config.json\'')
+    sys.exit(1)
 
 photo_dir = json_data['cornPhotoDir']
 output_dir = json_data['contourPhotoDir']
 
 if __name__ == '__main__':
-	main(args)
+    main(args)
