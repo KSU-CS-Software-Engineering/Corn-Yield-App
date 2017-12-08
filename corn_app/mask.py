22f4edf329a07ca885db03b0abe039bacce6ce50
Shawn Huggins <hugginsjrshawn@gmail.com>
2017-12-08 10:33:24 -0600

    Convert tabs to spaces.

--- corn_app/mask.py
+++ corn_app/mask.py
@@ -1,8 +1,8 @@
 """ Converts all image pixels not in the required HSV range to black
 
 Attributes:
-	LOWER_BOUND (array): HSV values for the lower boundary of color
-	UPPER_BOUND (array): HSV values for the upper boundary of color
+    LOWER_BOUND (array): HSV values for the lower boundary of color
+    UPPER_BOUND (array): HSV values for the upper boundary of color
 """
 import cv2
 import numpy as np
@@ -12,27 +12,27 @@ LOWER_BOUND_YELLOW = [20,100,100]
 UPPER_BOUND_YELLOW = [40,255,255]
 
 def mask_yellow(image):
-	"""Converts all image pixels not in the yellow HSV range to black
+    """Converts all image pixels not in the yellow HSV range to black
 
-	Args:
-		image (openCV Image): An open Image object.
+    Args:
+        image (openCV Image): An open Image object.
 
-	Returns:
-		bgr_yellow_image (openCV Image): An Image with yellow pixels extracted
-	"""
+    Returns:
+        bgr_yellow_image (openCV Image): An Image with yellow pixels extracted
+    """
 
-	if image is None:
-		return None
+    if image is None:
+        return None
 
-	hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	blur = cv2.GaussianBlur(hsv_image, (5,5), 0)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    blur = cv2.GaussianBlur(hsv_image, (5,5), 0)
 
-	lower_yellow = np.array(LOWER_BOUND_YELLOW)
-	upper_yellow = np.array(UPPER_BOUND_YELLOW)
+    lower_yellow = np.array(LOWER_BOUND_YELLOW)
+    upper_yellow = np.array(UPPER_BOUND_YELLOW)
 
-	yellow_mask = cv2.inRange(blur, lower_yellow, upper_yellow)
+    yellow_mask = cv2.inRange(blur, lower_yellow, upper_yellow)
 
-	hsv_yellow_image = cv2.bitwise_and(image, image, mask = yellow_mask)
-	bgr_yellow_image = cv2.cvtColor(hsv_yellow_image, cv2.COLOR_HSV2BGR)
+    hsv_yellow_image = cv2.bitwise_and(image, image, mask = yellow_mask)
+    bgr_yellow_image = cv2.cvtColor(hsv_yellow_image, cv2.COLOR_HSV2BGR)
 
-	return bgr_yellow_image
+    return bgr_yellow_image
