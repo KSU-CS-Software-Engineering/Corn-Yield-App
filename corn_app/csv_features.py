4aecdf6d85265a548e42f56b6126f04dc3f9e867
Shawn Huggins <hugginsjrshawn@gmail.com>
2018-01-23 14:59:48 -0600

    Ear features are now saved to a csv file during processing.

---
+++ corn_app/csv_features.py
@@ -0,0 +1,12 @@
+"""Module of constants pertaining to the csv file containing the ear features
+Attributes:
+    FILENAME  (str): Filename of the ear feature csv file
+    HEADER    (str): Header of the csv file
+    DELIM     (str): Delimiter character used for the csv file
+    QUOTECHAR (str): Character used for quotation marks
+"""
+
+FILENAME  = 'calculated_features.csv'
+HEADER    = ['image filename', 'front facing kernel count']
+DELIM     = '|'
+QUOTECHAR = '/'
